22b0b6c7b6cc7ad248010d3b99d566a2664d2ab5
Gustavolps7 <98482311+Gustavolps7@users.noreply.github.com>
2022-05-21 21:07:37 -0300

    me add

---
+++ Controllers/ComparadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comparador.Controllers
+{
+    public class ComparadorController : Controller
+    {
+        public IActionResult Comparador()
+        {
+            return View();
+        }
+    }
+}
